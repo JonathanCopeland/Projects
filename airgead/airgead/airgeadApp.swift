5312630464588d9f0fc30ecae110dc96991beb5a
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2023-05-21 18:04:09 +0100

    Fruta

---
+++ airgead/airgead/airgeadApp.swift
@@ -0,0 +1,17 @@
+//
+//  airgeadApp.swift
+//  airgead
+//
+//  Created by Jonathan Copeland on 21/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct airgeadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
