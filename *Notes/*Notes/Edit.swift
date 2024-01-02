32efff901ef869d115de30094848c1810e7419e1
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2024-01-02 09:53:12 +0000

    Initial commit

---
+++ *Notes/*Notes/Edit.swift
@@ -0,0 +1,18 @@
+//
+//  Edit.swift
+//  *Notes
+//
+//  Created by Jonathan Copeland on 02/01/2024.
+//
+
+import SwiftUI
+
+struct Edit: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Edit()
+}
---
+++ *Notes/*Notes/_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  _NotesApp.swift
+//  *Notes
+//
+//  Created by Jonathan Copeland on 02/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct _NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
